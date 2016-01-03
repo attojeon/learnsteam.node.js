8e70bfd38331f9f3f886c6bc5b21c8e42760fc69
attojeon <atto.jeon@gmail.com>
2016-01-03 15:37:50 +0900

    my custom module usages

---
+++ main.js
@@ -0,0 +1,4 @@
+var module = require('./myModule.js');
+
+console.log('%d', module.rectArea(3, 4));
+console.log('%d', module.circleArea(10));
---
+++ myModule.js
@@ -0,0 +1,7 @@
+exports.rectArea = function(width, height) {
+	return width * height;
+};
+
+exports.circleArea = function( radius) {
+	return radius * radius * Math.PI;
+};
