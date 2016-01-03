b8fbd00e1a7df8e14a6f139d94e63c9fb9237932
attojeon <atto.jeon@gmail.com>
2016-01-03 17:17:49 +0900

    updated

--- http/server_2.js
+++ http/server_2.js
@@ -1,11 +1,11 @@
 //
 var net = require('./ato_net.js');
-var server = require('http');
+var httpd = require('http');
 var fs = require('fs');
 var ip = net.getIPAddress();
 var port = 31415;
 
-server.createServer(function(request, response) {
+httpd.createServer(function(request, response) {
     fs.readFile('home.html', function(error, data) {
         response.writeHead(200, {
             'Content-Type': 'text/html'
