a0849468bc8e339e832e31c6b87733a950c60e2a
attojeon <atto.jeon@gmail.com>
2016-01-05 12:07:40 +0900

    remove error messages

--- gpio/dht11Run.py
+++ gpio/dht11Run.py
@@ -7,10 +7,7 @@ GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.cleanup()
 
-# read data using pin 14
-###########################################
-# pin이 물리적인 핀번호가 아님, gpio 포트 번호를 말하는 것임. 
-###########################################
+
 instance = dht11.DHT11(pin = 23)
 
 while( 1 ) :
@@ -20,6 +17,7 @@ while( 1 ) :
 	   	print("Temperature: %d C" % result.temperature)
 	    	print("Humidity: %d %%" % result.humidity)
 	else:
-	    	print("Error: %d" % result.error_code)
+		continue
+		# print("Error: %d" % result.error_code)
 
 	time.sleep(0.5)
